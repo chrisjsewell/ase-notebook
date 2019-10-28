a5aa6e11197115b17adbeb2e2a070673d5bd710b
Chris Sewell <chrisj_sewell@hotmail.com>
2019-10-28 19:55:47 +0000

    tidy visualize module

--- test_gui.py
+++ test_gui.py
@@ -4,7 +4,7 @@ import sys
 
 import pytest
 
-from aiida_2d.visualize import svg
+from aiida_2d.visualize import viewer
 
 
 @pytest.mark.skipif(
@@ -14,14 +14,14 @@ from aiida_2d.visualize import svg
 def test_make_gui(get_test_structure):
     """Test ``view_atoms_snapshot``."""
     structure = get_test_structure("pyrite")
-    viewer = svg.AseView(
+    ase_view = viewer.AseView(
         miller_planes=[
             {"index": [1, 1, 0], "color": "blue", "stroke_width": 1, "as_poly": False},
             {"index": [1, 2, 1], "color": "green", "stroke_width": 1, "as_poly": True},
         ],
         show_bonds=True,
     )
-    gui = viewer.make_gui(structure, launch=False)
+    gui = ase_view.make_gui(structure, launch=False)
     try:
         pass
     finally:
