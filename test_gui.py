3ed77b2f93c43c600aca3aec9addad75f1c512fd
Chris Sewell <chrisj_sewell@hotmail.com>
2019-10-28 19:42:47 +0000

    Move GUI launching to AseView

--- test_gui.py
+++ test_gui.py
@@ -2,126 +2,27 @@
 import os
 import sys
 
-import numpy as np
 import pytest
 
-from aiida_2d.visualize import core, gui
-
-
-def test_get_cell_coordinates():
-    """Test coordinates of the cell lines are computed correctly."""
-    line_starts, line_ends = core.get_cell_coordinates(
-        np.array([[1, 0, 0], [0, 2, 0], [0, 0, 3]])
-    )
-    # print(line_starts.tolist())
-    # print(line_ends.tolist())
-    assert line_starts.tolist() == [
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        [1.0, 0.0, 0.0],
-        [1.0, 2.0, 0.0],
-        [1.0, 0.0, 0.0],
-        [0.0, 0.0, 3.0],
-        [0.0, 2.0, 0.0],
-        [0.0, 0.0, 3.0],
-        [1.0, 2.0, 0.0],
-        [1.0, 0.0, 3.0],
-        [1.0, 2.0, 3.0],
-    ]
-    assert line_ends.tolist() == [
-        [1.0, 0.0, 0.0],
-        [0.0, 2.0, 0.0],
-        [0.0, 0.0, 3.0],
-        [1.0, 2.0, 0.0],
-        [0.0, 2.0, 0.0],
-        [1.0, 0.0, 3.0],
-        [1.0, 0.0, 3.0],
-        [0.0, 2.0, 3.0],
-        [0.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [0.0, 2.0, 3.0],
-    ]
-
-
-def test_get_cell_coordinates_2d():
-    """Test that cells with a zero length vector return a plane."""
-    line_starts, line_ends = core.get_cell_coordinates(
-        np.array([[1, 0, 0], [0, 2, 0], [0, 0, 0]])
-    )
-    # print(line_starts.tolist())
-    # print(line_ends.tolist())
-    assert line_starts.tolist() == [
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        [1.0, 0.0, 0.0],
-        [1.0, 2.0, 0.0],
-    ]
-    assert line_ends.tolist() == [
-        [1.0, 0.0, 0.0],
-        [0.0, 2.0, 0.0],
-        [1.0, 2.0, 0.0],
-        [0.0, 2.0, 0.0],
-    ]
-
-
-@pytest.mark.parametrize(
-    "miller,expected",
-    (
-        (
-            [2, 0, 0],
-            [[0.5, 0.0, 0.0], [0.5, 2.0, 0.0], [0.5, 2.0, 3.0], [0.5, 0.0, 3.0]],
-        ),
-        (
-            [0, 4, 0],
-            [[0.0, 0.5, 0.0], [1.0, 0.5, 0.0], [1.0, 0.5, 3.0], [0.0, 0.5, 3.0]],
-        ),
-        (
-            [0, 0, 3],
-            [[0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [1.0, 2.0, 1.0], [0.0, 2.0, 1.0]],
-        ),
-        (
-            [2, 1, 0],
-            [[0.5, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, 2.0, 3.0], [0.5, 0.0, 3.0]],
-        ),
-        ([1, 1, 1], [[1.0, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, 0.0, 3.0]]),
-    ),
-)
-def test_get_miller_coordinates(miller, expected):
-    """Test miller intercepts of the cell are computed correctly."""
-    points = core.get_miller_coordinates([[1, 0, 0], [0, 2, 0], [0, 0, 3]], miller)
-    assert points.tolist() == expected
+from aiida_2d.visualize import svg
 
 
 @pytest.mark.skipif(
     sys.platform != "darwin" and not os.environ.get("DISPLAY", False),
     reason="no display available for gui",
 )
-def test_snapshot_pil(get_test_structure):
+def test_make_gui(get_test_structure):
     """Test ``view_atoms_snapshot``."""
-    from PIL.EpsImagePlugin import EpsImageFile
-
     structure = get_test_structure("pyrite")
-    image = gui.view_atoms_snapshot(
-        structure,
-        miller_planes=[(1, 1, 0, "blue", 1, False), (1, 1, 1, "green", 1, True)],
+    viewer = svg.AseView(
+        miller_planes=[
+            {"index": [1, 1, 0], "color": "blue", "stroke_width": 1, "as_poly": False},
+            {"index": [1, 2, 1], "color": "green", "stroke_width": 1, "as_poly": True},
+        ],
+        show_bonds=True,
     )
-    assert isinstance(image, EpsImageFile), image
-
-
-@pytest.mark.skipif(
-    sys.platform != "darwin" and not os.environ.get("DISPLAY", False),
-    reason="no display available for gui",
-)
-def test_snapshot_ipy(get_test_structure):
-    """Test ``view_atoms_snapshot``."""
-    from IPython.display import SVG
-
-    structure = get_test_structure("pyrite")
-    image = gui.view_atoms_snapshot(
-        structure,
-        miller_planes=[(1, 1, 0, "blue", 1, False), (1, 1, 1, "green", 1, True)],
-        out_format="svg_ipy",
-    )
-    assert isinstance(image, SVG), image
+    gui = viewer.make_gui(structure, launch=False)
+    try:
+        pass
+    finally:
+        gui.exit()
