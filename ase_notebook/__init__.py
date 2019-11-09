c83f5d48bc5b17302777c93c95ef37e3cf445d10
Chris Sewell <chrisj_sewell@hotmail.com>
2019-11-09 03:41:12 +0000

    test fix

--- ase_notebook/__init__.py
+++ ase_notebook/__init__.py
@@ -1,7 +1,12 @@
 """Module for visualizing atomic configurations."""
-from .backend.svg import concatenate_svgs, svg_to_pdf  # noqa: F401
-from .color import Color  # noqa: F401
-from .configuration import ViewConfig  # noqa: F401
-from .viewer import AseView  # noqa: F401
+try:
+    from .backend.svg import concatenate_svgs, svg_to_pdf  # noqa: F401
+    from .color import Color  # noqa: F401
+    from .configuration import ViewConfig  # noqa: F401
+    from .viewer import AseView  # noqa: F401
+except ImportError:
+    # this is required by setup.py
+    # before dependencies are installed
+    pass
 
 __version__ = "0.1.0"
