cf5c58d5c0f89b46327e642082792ceb7b46b2c5
Chris Sewell <chrisj_sewell@hotmail.com>
2020-01-13 14:52:00 +0000

    Add options for selecting which bonds to show

--- tests/test_draw_utils.py
+++ tests/test_draw_utils.py
@@ -1,4 +1,5 @@
 """Tests for ``ase_notebook.gui``."""
+from ase import Atoms
 import numpy as np
 import pytest
 
@@ -91,3 +92,91 @@ def test_get_miller_coordinates(miller, expected):
         [[1, 0, 0], [0, 2, 0], [0, 0, 3]], miller
     )
     assert points.tolist() == expected
+
+
+def test_compute_bonds():
+    """Test bonds are correctly computed (no PBC)."""
+    atoms = Atoms(
+        numbers=[1, 2, 3],
+        positions=[[0, 0, 0], [0.2, 0.2, 0.2], [0.5, 0.5, 0.5]],
+        cell=np.eye(3),
+        pbc=False,
+    )
+    atom_radii = np.array([0.4, 0.4, 0.4])
+
+    bonds = draw_utils.compute_bonds(atoms, atom_radii)
+    # print(bonds.tolist())
+    assert np.allclose(bonds, [[0, 1, 0, 0, 0], [0, 2, 0, 0, 0], [1, 2, 0, 0, 0]])
+
+
+def test_compute_bonds_pbc():
+    """Test bonds are correctly computed (with PBC)."""
+    atoms = Atoms(numbers=[1], positions=[[0, 0, 0]], cell=np.eye(3), pbc=True)
+    atom_radii = np.array([0.4])
+
+    bonds = draw_utils.compute_bonds(atoms, atom_radii)
+    # print(bonds.tolist())
+    assert np.allclose(
+        bonds,
+        [
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, -1],
+            [0, 0, 0, -1, 0],
+            [0, 0, -1, 0, 0],
+        ],
+    )
+
+
+def test_compute_bonds_filter_by_index():
+    """Test filtering of bonds by atom indices."""
+    atoms = Atoms(
+        numbers=[1, 2], positions=[[0, 0, 0], [0.5, 0.5, 0.5]], cell=np.eye(3), pbc=True
+    )
+    # atoms.set_array("bonds", np.array([False, True]))
+    atom_radii = np.array([0.4, 0.4])
+    bonds = draw_utils.compute_bonds(atoms, atom_radii)
+    bonds = draw_utils.filter_bond_indices(bonds, [False, True])
+
+    # print(bonds.tolist())
+    assert np.allclose(
+        bonds,
+        [
+            [0, 1, 0, 0, 0],
+            [1, 0, 0, 0, 1],
+            [1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0],
+            [1, 1, 0, 1, 0],
+            [1, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0],
+            [1, 1, 1, 0, 0],
+            [1, 0, 1, 0, 1],
+            [1, 0, 1, 1, 0],
+            [1, 0, 1, 1, 1],
+            [1, 1, 0, 0, -1],
+            [1, 1, 0, -1, 0],
+            [1, 1, -1, 0, 0],
+        ],
+    )
+
+
+def test_compute_bonds_filter_element_pairs():
+    """Test filtering of bonds by element pairs."""
+    atoms = Atoms(
+        symbols=["H", "He", "Mg"],
+        positions=[[0, 0, 0], [0.2, 0.2, 0.2], [0.5, 0.5, 0.5]],
+        cell=np.eye(3),
+        pbc=False,
+    )
+    atom_radii = np.array([0.4, 0.4, 0.4])
+
+    bonds = draw_utils.compute_bonds(atoms, atom_radii)
+
+    allowed = [("H", "Mg")]
+    allowed = set([(a, b) for a, b in allowed] + [(b, a) for a, b in allowed])
+    symbols = atoms.get_chemical_symbols()
+    bonds = bonds[[(symbols[i], symbols[j]) in allowed for i, j in bonds[:, 0:2]]]
+
+    # print(bonds.tolist())
+    assert np.allclose(bonds, [[0, 2, 0, 0, 0]])
